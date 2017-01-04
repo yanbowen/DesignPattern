d5bf8968a35b54ccda54c95d83516797bc4312a7
闫博文 <yanbowen90@outlook.com>
2017-01-04 12:16:52 +0800

    .添加开闭原则

--- src/OpenClosePrinciple/BookStore.java
+++ src/OpenClosePrinciple/BookStore.java
@@ -10,10 +10,15 @@ public class BookStore {
     private final static ArrayList<IBook> bookList = new ArrayList<>();
 
     static {
-        bookList.add(new NovelBook("天龙八部", 3200, "金庸"));
-        bookList.add(new NovelBook("巴黎圣母院", 5600, "雨果"));
-        bookList.add(new NovelBook("悲惨世界", 3500, "雨果"));
-        bookList.add(new NovelBook("金瓶梅", 4300, "兰陵笑笑生"));
+//        bookList.add(new NovelBook("天龙八部", 3200, "金庸"));
+//        bookList.add(new NovelBook("巴黎圣母院", 5600, "雨果"));
+//        bookList.add(new NovelBook("悲惨世界", 3500, "雨果"));
+//        bookList.add(new NovelBook("金瓶梅", 4300, "兰陵笑笑生"));
+
+        bookList.add(new offNovelBook("天龙八部", 3200, "金庸"));
+        bookList.add(new offNovelBook("巴黎圣母院", 5600, "雨果"));
+        bookList.add(new offNovelBook("悲惨世界", 3500, "雨果"));
+        bookList.add(new offNovelBook("金瓶梅", 4300, "兰陵笑笑生"));
     }
 
     public static void main(String[] args) {
@@ -22,7 +27,7 @@ public class BookStore {
         System.out.println("-----------------书店卖出去的书籍记录如下：-----------------");
 
         for (IBook book : bookList) {
-            System.out.println("书籍名称： " + book.getName() + "\t书籍作者： " + book.getAuthor() + "\t书籍价格： " + NumberFormat.format(book.getPrice() / 100.0) + "元");
+            System.out.println("书籍名称： \t" + book.getName() + "\t书籍作者： \t" + book.getAuthor() + "\t书籍价格： " + NumberFormat.format(book.getPrice() / 100.0) + "元");
         }
     }
 }
---
+++ src/OpenClosePrinciple/offNovelBook.java
@@ -0,0 +1,22 @@
+package OpenClosePrinciple;
+
+/**
+ * Created by yanbowen on 1/4/2017.
+ */
+public class offNovelBook extends NovelBook {
+    offNovelBook(String _name, int _price, String _author) {
+        super(_name, _price, _author);
+    }
+
+    @Override
+    public int getPrice() {
+        int selfPrice = super.getPrice();
+        int offPrice = 0;
+        if (selfPrice > 4000) {
+            offPrice = selfPrice * 90 / 100;
+        } else {
+            offPrice = selfPrice * 80 / 100;
+        }
+        return offPrice;
+    }
+}
