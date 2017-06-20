62afee9cf4b4b47e01be54c1e3be30e196d62bad
闫博文 <yanbowen90@outlook.com>
2017-06-20 15:17:32 +0800

    观察者模式代码更新

--- src/TwentyThreeKindsOfDesignPatterns/ObserverPattern/Client.java
+++ src/TwentyThreeKindsOfDesignPatterns/ObserverPattern/Client.java
@@ -12,5 +12,21 @@ public class Client {
         HanFeiZi HanFeiZi = new HanFeiZi();
         HanFeiZi.addObserver(liSi);
         HanFeiZi.haveBreakfast();
+        double d = 10.123456789;
+        float d2 = (float) 10.896;
+        System.out.println( d + "\n" + d2);
+
+        TreeSet<Integer> subs = new TreeSet<Integer>();
+        subs.add(99);
+        subs.add(29);
+        subs.add(57);
+        System.out.println("\n" + subs);
+
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        a.add(1); a.add(5);a.add(3);a.add(3);a.add(39);a.add(3);
+        Collections.sort(a);
+        a.add(2);
+        Collections.reverse(a);
+        System.out.println("\n" + a);
     }
 }
